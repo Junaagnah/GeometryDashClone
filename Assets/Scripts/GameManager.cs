00df1281f265fb8f011eaae79036ed7dded9c64f
GEHRINGER Nicolas <nicolas.gehringer@outlook.fr>
2022-02-25 18:30:58 +0100

    Finished basic menu, pause & game over systems

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,55 +8,108 @@ public class GameManager : MonoBehaviour
 {
     public Canvas StartingCount;
     public Canvas Menu;
+    public Canvas GameOver;
 
     private bool gameStarted = false;
     private GameObject Player;
-
     private float elapsedTime = 0f;
+    private bool isPaused = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         // Stopping player movement
+        //Player.GetComponent<Rigidbody>().collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+        //Player.GetComponent<Rigidbody>().isKinematic = true;
+        Player.GetComponent<Player>().gamePaused = true;
 
+        GameOver.enabled = false;
         Menu.enabled = false;
         StartingCount.enabled = true;
+
+        // Hiding 2 & 1 texts
+        StartingCount.transform.GetChild(1).GetComponent<Text>().enabled = false;
+        StartingCount.transform.GetChild(2).GetComponent<Text>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         elapsedTime += Time.deltaTime;
-        Debug.Log(elapsedTime);
 
         if (!gameStarted)
-        {  
-            // Countdown starts at 2 secondes after the scene is loaded
-            switch(elapsedTime)
-            {
-                case 3.0f:
-                    StartingCount.GetComponents<Text>()[0].enabled = false;
-                    StartingCount.GetComponents<Text>()[1].enabled = true;
-                    break;
-                case 4.0f:
-                    StartingCount.GetComponents<Text>()[1].enabled = false;
-                    StartingCount.GetComponents<Text>()[2].enabled = true;
-                    break;
-                case 5.0f:
-                    StartingCount.GetComponents<Text>()[2].enabled = false;
-                    // Starting player movement
-                    gameStarted = true;
-                    break;
-            }
-
+        {
+            CountDown();
         } else
         {
+            if (Player != null)
+            {
+                // Pause
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    Pause();
+                }
+            } else
+            {
+                // If the gameobject player is null then he is dead, show the Game Over canvas
+                GameOver.enabled = true;
+            }
+
             // Reset the level when the player presses R
             if (Input.GetKeyDown(KeyCode.R))
             {
                 SceneManager.LoadScene("Game");
             }
         }
+    }
+
+    private void CountDown()
+    {
+        //Player.GetComponent<Player>().shouldMove = false;
+        // Countdown starts at 2 secondes after the scene is loaded
+        if (elapsedTime >= 1.0f && elapsedTime < 2.0f)
+        {
+            StartingCount.transform.GetChild(0).GetComponent<Text>().enabled = false;
+            StartingCount.transform.GetChild(1).GetComponent<Text>().enabled = true;
+        }
+        else if (elapsedTime >= 2.0f && elapsedTime < 3.0f)
+        {
+            StartingCount.transform.GetChild(1).GetComponent<Text>().enabled = false;
+            StartingCount.transform.GetChild(2).GetComponent<Text>().enabled = true;
+        }
+        else if (elapsedTime >= 3.0f)
+        {
+            StartingCount.transform.GetChild(2).GetComponent<Text>().enabled = false;
+            // Starting player movement
+            //Player.GetComponent<Rigidbody>().isKinematic = false;
+            //Player.GetComponent<Rigidbody>().collisionDetectionMode = CollisionDetectionMode.Continuous;
+            Player.GetComponent<Player>().gamePaused = false;
+            gameStarted = true;
+        }
+    }
+
+    public void Pause()
+    {
+        if (!isPaused)
+        {
+            isPaused = true;
+            Time.timeScale = 0;
+            Menu.enabled = true;
+        }
+        else
+        {
+            isPaused = false;
+            Time.timeScale = 1;
+            Menu.enabled = false;
+        }
+
+        Player.GetComponent<Player>().gamePaused = isPaused;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public ParticleSystem Explosion;
+    public bool gamePaused = false;
 
     [SerializeField]
     private int speed = 15;
@@ -13,7 +14,6 @@ public class Player : MonoBehaviour
     private float gravityScale = 10;
 
     private bool canJump = false;
-    private bool shouldMove = false;
     private int nbJumps = 0; // Double jump
 
     private Rigidbody rb;
@@ -27,7 +27,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (shouldMove)
+        if (!gamePaused)
         {
             // Auto movement
             transform.position += speed * Time.deltaTime * Vector3.right;
@@ -55,8 +55,6 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (!shouldMove) shouldMove = true; // Avoid resetting the value each time there is a collision
-
         rb.velocity = Vector3.zero;
         canJump = true;
         nbJumps = 0;
@@ -69,7 +67,6 @@ public class Player : MonoBehaviour
 
     public void KillPlayer()
     {
-        shouldMove = false;
         Instantiate(Explosion, gameObject.transform.position, Quaternion.identity);
         
         Explosion.Play();
